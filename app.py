c2a56a0e726b5ca377b1fbf8494c8341eb3cf186
accakks <aakanksha.jain8@gmail.com>
2020-05-11 09:13:32 +0530

    v1:Create works with form input, server decisions, reload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://akku:akku@localhost:5432/todoapp'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+
+@app.route('/todos/create', methods=['POST'])
+def create():
+    description = request.form.get('description')
+    new_item = Todo(description = description)
+    db.session.add(new_item)
+    db.session.commit()
+    return redirect(url_for('index'))
