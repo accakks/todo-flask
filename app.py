03288d508de4048cd9788a6ff4b5317e9f5fa853
accakks <aakanksha.jain8@gmail.com>
2020-05-12 03:06:42 +0530

    v6 : Delete added + update fixed

--- app.py
+++ app.py
@@ -6,17 +6,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://akku:akku@localhost:5432/todoapp'
 
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default = False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-#db.create_all()
+# db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -29,21 +30,22 @@ def create_todo():
         db.session.add(new_item)
         db.session.commit()
         body['description'] = new_item.description
-      
-    except: 
+
+    except:
         error = True
         db.session.rollback()
         print(sys.exc_info())
     finally:
         db.session.close()
     if error:
-        abort (400)
+        abort(400)
     if not error:
         return jsonify(body)
 
+
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
-    try: 
+    try:
         completed = request.get_json()['completed']
         todo = Todo.query.get(todo_id)
         todo.completed = completed
@@ -54,14 +56,21 @@ def set_completed_todo(todo_id):
         db.session.close()
     return redirect(url_for('index'))
 
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
 
 
 @app.route('/')
 def index():
-    print()
     return render_template('index.html', data=Todo.query.order_by('id').all())
-    
-        
 
 
 if __name__ == "__main__":
